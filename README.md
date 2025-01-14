5603de2f43abb028d1c91f3450b30bc570f48ca5
Kamila <kamilascfgomes@gmail.com>
2025-01-14 11:31:37 -0300

    Entidade pronta

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # clp
 
+Grupo:
+    Daniel Chateaubriand Lago
+    Kamila Santos de Camargos Ferreira Gomes
+    Marina Rihs Matos Wang
+    Pedro Henrique Assis Dornelas
+    Wesley dos Santos Gomes
+
+Linguagem utilizada: Julia
+
 Explicações sobre a linguagem foram adicionadas em forma de comentários ao longo do código. Para melhor entendimento, recomenda-se ler os arquivos na seguinte ordem:
 
 1. Entidades/entidade.jl
 2. Entidades/produto.jl
 
 No fim de cada arquivo será indicado qual o arquivo seguinte a ser lido.
+
+O código pode ser executado linha a linha, selecionando as linhas desejadas e utilizando o shift + enter, e então as linhas executadas são armazenadas na REPL (Read-Eval-Print Loop)
+
+A formatação do código utiliza a palavra end para demarcar o fim do escopo de funções. A tabulação não é importante, mas é utilizada para facilitar a compreenção
--- tp julia/Entidades/Entidade.jl
+++ tp julia/Entidades/Entidade.jl
@@ -1,38 +1,34 @@
 using Dates
 # biblioteca Dates importada para utilizar a função now
 
-#   struct é uma estrutura imutável
-#   mutable struct é uma estrutura mutável
+#=
+struct é uma estrutura imutável, mutable struct é uma estrutura
+mutável
+
+tipagem paramétrica flexível: tipagem estática é opcional e
+tipos podem receber parâmetros, pode definir tamanhos =#
 struct Entidade
-    id
-    # tipagem paramétrica flexível: tipagem estática é opcional
-    # e tipos podem receber parâmetros, pode definir tamanhos
+    id:: Integer
 end
 
-function newEntidade() # NÃO TERMINEI ESSE
-    Dates.Millisecond(Dates.now())
-    aa = Entidade(now())
-    println(aa.id)
+#=
+declaração da função newEntidade de forma concisa:
+pega o tempo agora, converte para ms e utiliza o valor como id =#
+newEntidade() = Entidade(Dates.value(convert(Dates.Millisecond, Dates.now())))
 
-end
-# função getId não é necessária, já que a estrutura é imutável
-# mas permite ler seus valores
+#=
+função getId não é necessária, já que a estrutura é imutável
+mas permite ler seus valores =#
 
-# para adaptar uma linguagem orientada a objetos para uma
-# imperativa,uma função que pertenceria a entidade se torna uma
-# função que recebe uma entidade
+#=
+para concatenar basta chamar a variável utilizando $, como em
+Perl. Também realiza operações, ex "1 + 2 = $(1 + 2)"
 
+outra forma de declarar uma função:
+se o retorno não é especificado, será a última linha da função=#
 function toString(self::Entidade)
-    # para concatenar ou interpolar, basta chamar a variável
-    # desejada utilizando $, como em Perl. Também pode ser
-    # utilizado para realizar operações, ex "1 + 2 = $(1 + 2)"
-    # também funciona com vetores,onde que $v o imprime inteiro
     "Id: $(self.id)\t"
-
-    # não é necessário especificar o retorno, por padrão a
-    # última linha da função será definida como retorno
 end
 
-println(toString(Entidade(now()))) #teste
 
 # Próximo arquivo: Entidades/Produto.jl
