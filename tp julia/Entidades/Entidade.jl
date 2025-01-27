d6ed06ce534ee441dc36713932b63bec2c2a0ca1
Kamila <kamilascfgomes@gmail.com>
2025-01-27 10:58:30 -0300

    o modulo entidade funciona :D
    Co-authored-by: Pedro H Assis Dornelas <PedrohhDornelas@users.noreply.github.com>

--- tp julia/Entidades/Entidade.jl
+++ tp julia/Entidades/Entidade.jl
@@ -1,47 +1,34 @@
 module Entidade
-
-export EntDados
-export newEntDados
-
     using Dates
-    #= biblioteca Dates importada para utilizar as funções de
-    data e hora
+    # biblioteca Dates importada para utilizar a função now
+
+    #=
+    struct é uma estrutura imutável, mutable struct é uma estrutura
+    mutável
 
     tipagem paramétrica flexível: tipagem estática é opcional e
-    tipos podem receber parâmetros, pode definir tamanhos
-
-    tipos abstratos permitem organização de uma hierarquia,
-    mas eles não podem ter atributos então os atributos são
-    definidos em uma struct separada =#
-    
-    struct EntDados
-        id::Int64
+    tipos podem receber parâmetros, pode definir tamanhos =#
+    struct Entidadee
+        id:: Integer
     end
 
     #=
-    struct é uma estrutura imutável,
-    mutable struct é uma estrutura mutável
-
     declaração da função newEntidade de forma concisa:
     pega o tempo agora, converte para ms e utiliza o valor como id =#
+    newEntidade() = Entidadee(Dates.value(convert(Dates.Millisecond, Dates.now())))
 
-    newEntDados() = EntDados(Dates.value(convert(Dates.Millisecond, Dates.now())))
-
-    getId(self::EntDados) = self.id
     #=
     função getId não é necessária, já que a estrutura é imutável
-    mas permite ler seus valores
-    
+    mas permite ler seus valores =#
 
-    para concatenar utilizando interpolação basta chamar a variável
-    utilizando $, como em Perl. Tambem pode realizar opetações e funções
+    #=
+    para concatenar basta chamar a variável utilizando $, como em
+    Perl. Também realiza operações, ex "1 + 2 = $(1 + 2)"
 
     outra forma de declarar uma função:
     se o retorno não é especificado, será a última linha da função=#
-    function toString(self::EntDados)
+    function toString(self::Entidadee)
         "Id: $(self.id)\t"
     end
-
 end
-
 # Próximo arquivo: Entidades/Produto.jl
--- tp julia/Entidades/Produto.jl
+++ tp julia/Entidades/Produto.jl
@@ -1,22 +1,23 @@
-module Produto
-
-    include("Entidade.jl")
-    using .Entidade
+    include("./Entidade.jl") #inclui o diretório do arquivo
+    using .Entidade: Entidadee, newEntidade, toString #especifica o que será usado
+    import .Entidade.toString # é necessário para executar a sobrecarga
 #=
 mutable string é uma string mutavel
 <: é utilizado para indicar hierarquia
 o atributo EntDados simula a herança =#
     mutable struct ProDados
-        e::EntDados
+        e::Entidadee
         nome::String
         valor::Float64
     end
 
-    # sobrecarga da função newProDados:
-    newProDados() = ProDados(newEntDados(), "", 0.0)::ProDados
+    # envio dinâmico da função newProDados:
+    newProDados() = ProDados(newEntidade(), "", 0.0)::ProDados
+    newProDados(nome::String, valor::Float64)::ProDados = ProDados(newEntidade(), nome, valor)
+    #= semelhante a sobrecarga, mas a sobrecarga é definida em tempo
+    de compilação, enquanto o envio dinâmico é resolvido em tempo de
+    execução =#
     
-    newProDados(nome::String, valor::Float64)::ProDados = ProDados(newEntDados(), nome, valor)
-
     #a::ProDados = newProDados("Penis de 30cm",2.99)
     #b = toString(a)
 
@@ -30,5 +31,4 @@ o atributo EntDados simula a herança =#
 
     toString(self::ProDados) = "$(toString(self.e))Nome: $(self.nome)\tValor:$(self.valor)"
 
-end
 # Próximo arquivo: Entidades/Totalizavel.jl
